5a5a3404e263409a0d54856f2c951dc4c6e4187f
jeakaura <jeakaura@student.jyu.fi>
2026-08-20 12:24:34 +0000

    ex06

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,20 +1,49 @@
 import { getBlogs } from "../services/blogs"
 
-const Blogs = () => {
+const Blogs = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ filter?: string }>
+}) => {
+  const { filter } = await searchParams
   const blogs = getBlogs()
   return (
     <div>
       <h2>Blogs</h2>
-      <ul>
-        {blogs.map(blog => (
-          <li key={blog.id}>
-            <h3><a href={`/blogs/${blog.id}`}>{blog.title}</a></h3>
-            <p>Author: {blog.author}</p>
-            <p>Url: <a href={blog.url}>{blog.url}</a></p>
-            <p>Likes: {blog.likes}</p>
-          </li>
-        ))}
-      </ul>
+      
+      <form>
+        <input type="text" name="filter" placeholder="Filter blogs..." />
+        <button type="submit">Filter</button>
+      </form>
+
+      {
+        filter ? (
+          <ul>
+            {
+              blogs.filter(blog => blog.title.toLowerCase().includes(filter.toLowerCase())).map(blog => (
+                <li key={blog.id}>
+                  <h3><a href={`/blogs/${blog.id}`}>{blog.title}</a></h3>
+                  <p>Author: {blog.author}</p>
+                  <p>Url: <a href={blog.url}>{blog.url}</a></p>
+                  <p>Likes: {blog.likes}</p>
+                </li>
+              ))
+            }
+          </ul>
+        ) : (
+          <ul>
+            {blogs.map(blog => (
+              <li key={blog.id}>
+                <h3><a href={`/blogs/${blog.id}`}>{blog.title}</a></h3>
+                <p>Author: {blog.author}</p>
+                <p>Url: <a href={blog.url}>{blog.url}</a></p>
+                <p>Likes: {blog.likes}</p>
+              </li>
+            ))}
+          </ul>
+        )
+      }
+
     </div>
   )
 }
